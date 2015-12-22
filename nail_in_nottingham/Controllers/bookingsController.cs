fa369afe68a2809266f54d0519796b8a8c93cd16
indranimukherjee <indrani.emedia@outlook.com>
2015-12-22 11:44:32 +0530

    initial commit

--- nail_in_nottingham/Controllers/bookingsController.cs
+++ nail_in_nottingham/Controllers/bookingsController.cs
@@ -27,8 +27,11 @@ namespace nail_in_nottingham.Controllers
 
             List<SelectedItem> selectedItemList = new List<SelectedItem>();
 
-            for (int i = 0; i < 15; i++) {
-                selectedItemList.Add(new SelectedItem());               
+            //Total No of serviceItems selected
+            for (int i = 0; i < 15; i++)
+            {
+                selectedItemList.Add(new SelectedItem());
+
             }
 
 
@@ -38,7 +41,8 @@ namespace nail_in_nottingham.Controllers
                 ServiceBillDesk serviceBillDeskItem = db.ServiceBillDesks.Find(SBId);
                 if (serviceBillDeskItem != null)
                 {
-                    //SelectedItem item = new SelectedItem();
+
+                    // SelectedItem item = new SelectedItem();
                     selectedItemList[0].ServiceName = serviceBillDeskItem.ServiceMaster.ServiceType;
                     selectedItemList[0].serviceVeriation = serviceBillDeskItem.ServiceVeriation.ServiceVariation;
                     selectedItemList[0].timeDuration = serviceBillDeskItem.ServiceDuration;
@@ -46,8 +50,9 @@ namespace nail_in_nottingham.Controllers
                     selectedItemList[0].NoOfPerson = serviceBillDeskItem.ServiceQuantity;
                     selectedItemList[0].ServiceBillDeskId = serviceBillDeskItem.ID;
                     //selectedItemList.Add(item);
-                    ViewBag.IsItems = true;
+
                     //ViewBag.ServiceItem = serviceBillDeskItem;
+                    ViewBag.IsItems = true;
                 }
 
             }
@@ -70,55 +75,40 @@ namespace nail_in_nottingham.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(BookingViewModel bookinfo)
+
+        public ActionResult Index(List<SelectedItem> selItems, booking bookingDetails, FormCollection frm)
         {
+            string tset = Convert.ToString(frm["testValue"]);
             BookingViewModel bookingViewModel = new BookingViewModel();
 
             bookingViewModel.bookingServices = new booking();
 
             bookingViewModel.serviceBillDeskDetails = db.ServiceBillDesks.ToList();
 
-            bookingViewModel.SelectedItem = null;
-           
-            return View(bookingViewModel);
+            bookingViewModel.SelectedItem = selItems;
 
+            return View(bookingViewModel);
         }
+
+
+        [HttpPost]
+        public JsonResult Submit(booking bookingDetails)
+        {
+            booking bookDetailsItems = new booking()
+            {
+                consumer_first_name = bookingDetails.consumer_first_name,
+                consumer_last_name = bookingDetails.consumer_last_name,
+                consumer_email = bookingDetails.consumer_email,
+                consumer__mobileno = bookingDetails.consumer__mobileno,
+                booking_start_datetime = bookingDetails.booking_start_datetime,
+                booking_end_datetime = bookingDetails.booking_end_datetime,
+                TotalQuantity = bookingDetails.TotalQuantity,
+                TotalAmount = bookingDetails.TotalAmount
+            };
+            db.bookings.Add(bookingDetails);
+            db.SaveChanges();
+            return Json("Response from Create" + bookDetailsItems.ID);
+        }
+
     }
 }
-//[HttpPost]
-//        [ValidateAntiForgeryToken]
-        //public ActionResult Index(booking BOOK, FormCollection frm, string SlotType)
-        //{
-        //    //int serviceId = Convert.ToInt32(frm["Servicelist"].ToString());
-
-        //    if (ModelState.IsValid)
-        //    {
-
-        //        db.bookings.Add(new booking
-        //        {
-        //            //booking_id = BOOK.booking_id,
-        //            consumer_first_name = BOOK.consumer_first_name,
-        //            consumer_last_name = BOOK.consumer_last_name,
-        //            consumer_email = BOOK.consumer_email,
-        //            consumer__mobileno = BOOK.consumer__mobileno,
-        //            //service_id = serviceId,
-        //            booking_date = Convert.ToDateTime(BOOK.booking_date),
-        //            SlotID = Convert.ToInt32(BOOK.SlotID),
-        //            //BillDeskID = Convert.ToInt32(BOOK.ServiceBillDeskID),
-        //            TotalAmount = BOOK.TotalAmount,
-        //            TotalQuantity = Convert.ToInt32(BOOK.TotalQuantity)
-        //        });
-
-        //        int i = db.SaveChanges();
-        //        if (i > 0)
-        //        {
-        //            ViewBag.ResultMessage = "BOOKING SUCCESSFUL";
-        //        }
-
-        //        return RedirectToAction("Index");
-        //    }
-
-
-        //    return View(BOOK);
-
-        //}
